7d906cb8b5a207e0fe01681cf6fd5fe1609e4384
RaceWilliams3 <willr108@my.erau.edu>
2023-04-23 21:58:05 -0700

    I need to sleep but Don't want to lose anything

--- MachineLearning/Assets/Scripts/AvoidObstacleAgent.cs
+++ MachineLearning/Assets/Scripts/AvoidObstacleAgent.cs
@@ -3,12 +3,53 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class AvoidObstacleAgent : Agent
 {
-    
-    public override void OnActionReceived(ActionBuffers action)
-    {
+    public PlayerController pc;
 
+    private Vector3 startPos;
+
+    void Start()
+    {
+        startPos = transform.position;
+    }
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = startPos;
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        base.CollectObservations(sensor);
+    }
+    
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        pc.updateInput(actions.ContinuousActions[0]);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-1);
+        }
+        if (collision.gameObject.CompareTag("Goal"))
+        {
+            AddReward(1);
+            EndEpisode();
+
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            AddReward(-0.1f);
+        }
     }
 }
--- MachineLearning/Assets/Scripts/PlayerController.cs
+++ MachineLearning/Assets/Scripts/PlayerController.cs
@@ -12,11 +12,7 @@ public class PlayerController : MonoBehaviour
     public void updateInput(float leftRight)
     {
         rb.AddForce((Vector3.right * leftRight) * moveSpeed * Time.deltaTime, ForceMode.VelocityChange);
-    }
-
-    void Update()
-    {
-        updateInput(Input.GetAxis("Horizontal"));
         rb.AddForce(0, 0, acceleration, ForceMode.Acceleration);
     }
+
 }
